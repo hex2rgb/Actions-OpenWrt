9549d1aaa35878722529f3cee09b31218e2d6c0d
robert <robert196@163.com>
2026-01-20 18:48:24 +0800

    fix: 更新
    - modified config
    -  .github/workflows/openwrt-builder.yml
    -  diy-part1.sh
    -  diy-part2.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,13 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Add feed sources for required packages
+# Passwall feed (contains xray, hysteria2, hysteria, and more)
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# Add a feed source
+# Helloworld feed (alternative/additional packages)
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Kenzo feeds (contains smartdns and other useful packages)
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# Modify default theme to Argon (better UI)
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
